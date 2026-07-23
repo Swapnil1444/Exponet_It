02971a24a53e1a57762d671b34d501a657b980d9
Swapnil <swpnilsupekar@gmail.com>
2026-07-23 11:17:06 +0530

    type casting implicity

---
+++ Opp/src/com/typeCasting/ImplicityExamples.java
@@ -0,0 +1,42 @@
+package com.typeCasting;
+
+public class ImplicityExamples {
+
+	public static void main(String[] args) {
+		
+		//byte>short>char>int>long>flott>double>
+		
+		
+		byte b=120;
+		short s=b;
+		System.out.println(s);
+		
+		int i=s;
+		System.out.println(i);
+		
+		long l=i;
+		System.out.println(l);
+		
+		float f=l;
+		System.out.println(f);
+		
+		double d=f;
+		System.out.println(d);
+		
+		char c='A';
+		int i1=c;
+		System.out.println(i1);
+
+		long l1=c;
+		System.out.println(l1);
+		
+		float f1=c; 
+		System.out.println(f1);
+		
+		double d1=c;
+		System.out.println(d1);
+		
+
+	}
+
+}
